1b2159a4c6913af206a951e515142d707c5ea924
wxrdnx <wxrdnx@protonmail.com>
2023-03-16 11:07:04 +0800

    upgrade dns & web

--- bin/tuf_dns.py
+++ bin/tuf_dns.py
@@ -4,35 +4,35 @@ fake = '8.8.8.8'
 class DNSQuery:
     def __init__(self, data):
         self.data    = data
-        self.dominio = ''
+        self.dominio = b''
 
-        tipo = (ord(data[2]) >> 3) & 15 # Opcode bits
-        if tipo == 0:                   # Standard query
+        tipo = (data[2] >> 3) & 15 # Opcode bits
+        if tipo == 0: # Standard query
             ini = 12
-            lon = ord(data[ini])
+            lon = data[ini]
             while lon != 0:
-                self.dominio += data[ini+1:ini+lon+1] + '.'
+                self.dominio += data[ini+1:ini+lon+1] + b'.'
                 ini += lon+1
-                lon = ord(data[ini])
+                lon = data[ini]
 
     def respuesta(self, ip):
-        print [self.dominio]
+        print(self.dominio.decode())
 
-        packet = ''
+        packet = b''
         if self.dominio:
-            packet += self.data[:2] + "\x81\x80"
-            packet += self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00' # Questions and Answers Counts
+            packet += self.data[:2] + b"\x81\x80"
+            packet += self.data[4:6] + self.data[4:6] + b'\x00\x00\x00\x00' # Questions and Answers Counts
             packet += self.data[12:] # Original Domain Name Question
-            packet += '\xc0\x0c' # Pointer to domain name
-            packet += '\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04' # Response type, ttl and resource data length -> 4 bytes
-            packet += str.join('',map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
+            packet += b'\xc0\x0c' # Pointer to domain name
+            packet += b'\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04' # Response type, ttl and resource data length -> 4 bytes
+            packet += str.join('', map(lambda x: chr(int(x)), ip.split('.'))).encode() # 4bytes of IP
         return packet
 
 if __name__ == '__main__':
-    print 'fakeDNS:: dom.query. 60 IN A %s' % fake
+    print('fakeDNS:: dom.query. 60 IN A {0}'.format(fake))
     
     udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udps.bind(('',53))
+    udps.bind(('', 53))
     
     try:
         while 1:
--- bin/tuf_web.js
+++ bin/tuf_web.js
@@ -1,17 +1,16 @@
-var ip   = "0.0.0.0";
-var http = require('http');
-var util = require('util');
+const ip   = "0.0.0.0";
+const http = require('http');
+const util = require('util');
 // WHILE_LISTS = ['user-agent']
 
-port = process.argv[2];
+for (let port = 1; port < 49152; port++) {
+    http.createServer(function (req, res) {
+        url = req.connection.address().address + ':' + req.connection.address().port + req.url;
+        console.log(util.format("[%s] - http://%s - [%s]", req.connection.remoteAddress,
+            url, req.headers['user-agent']||''));
 
-http.createServer(function (req, res) {
-    url = req.connection.address().address + ':' + req.connection.address().port + req.url;
-    console.log(util.format("[%s] - http://%s - [%s]", req.connection.remoteAddress,
-                url, req.headers['user-agent']||''));
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(url + '\n');
-}).listen(port, ip);
-
-console.log("Server running at http://" + ip + ":" + port);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(url + '\n');
+    }).listen(port, ip);
+}
+console.log("Server running at http://" + ip + ":1-49151");
